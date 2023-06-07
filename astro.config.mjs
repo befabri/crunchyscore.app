6889f5e1200208ca52b0c5b92fda49d29b0becc1
Benjamin Fabri <contact@bfabri.me>
2023-06-07 03:59:08 +0200

    Add tos & privacy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,14 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 import sitemap from "@astrojs/sitemap";
+import mdx from "@astrojs/mdx";
 
+// https://astro.build/config
 export default defineConfig({
   site: "https://crunchyscore.app",
   outDir: "public",
   publicDir: "static",
-  integrations: [
-    tailwind(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-    sitemap(),
-  ],
+  integrations: [tailwind(), image({
+    serviceEntryPoint: "@astrojs/image/sharp"
+  }), sitemap(), mdx()]
 });
