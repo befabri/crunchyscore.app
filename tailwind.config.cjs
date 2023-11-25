ac0a2e7a6c3baeb398521452fa218bea135b7030
Benjamin Fabri <contact@bfabri.me>
2023-11-25 15:54:22 +0100

    Update CTA color

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,17 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  theme: {
-    extend: {
-      colors: {
-        custom_orange: "#F47521",
-        custom_blue: "#2F52A2",
-      },
-      fontFamily: {
-        sans: ["InterVariable", "Inter", ...defaultTheme.fontFamily.sans],
-      },
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    theme: {
+        extend: {
+            colors: {
+                custom_orange: "#F47521",
+                custom_blue: "#2F52A2",
+                custom_black: "#1F2421",
+                custom_night: "#141414",
+            },
+            fontFamily: {
+                sans: ["InterVariable", "Inter", ...defaultTheme.fontFamily.sans],
+            },
+        },
     },
-  },
-  plugins: [require("@tailwindcss/typography")],
+    plugins: [require("@tailwindcss/typography")],
 };
