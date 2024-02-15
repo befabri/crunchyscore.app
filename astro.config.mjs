fa26d167a05db0b590cfaa6c45d090dc9ee01e30
Benjamin Fabri <bfabri@pm.me>
2024-02-15 18:46:55 +0100

    Update astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import image from "@astrojs/image";
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://crunchyscore.app",
   outDir: "public",
   publicDir: "static",
-  integrations: [tailwind(), image({
-    serviceEntryPoint: "@astrojs/image/sharp"
-  }), sitemap(), mdx()]
+  integrations: [tailwind(), sitemap(), icon(), mdx()]
 });
